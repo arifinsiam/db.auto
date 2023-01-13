c6a25b68ef7dd72a1cf371db51a03e189c5fc340
arifinsiam <arifinsiam3211@gmail.com>
2023-01-13 15:41:53 +0600

    update test.sql

--- test.sql
+++ test.sql
@@ -1,8 +1,4 @@
-INSERT INTO invokeTable 
-VALUES (9,'I')
-  , (10,'J')
-  , (11,'K')
-  , (12,'L')
- 
-UPDATE invokeTable
-SET IdData = IdData + '-' + CAST(Id AS VARCHAR(1))
+DELETE FROM invokeTable WHERE Id = 9
+DELETE FROM invokeTable WHERE Id = 10
+DELETE FROM invokeTable WHERE Id = 11
+DELETE FROM invokeTable WHERE Id = 12
